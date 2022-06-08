31896a1bc789019ae0e1f86696ca31f8f2a4a58a
geraldgillespie <gerald.gillespie@inscio.com>
2022-06-08 06:24:42 -0400

    added in jsdoc plugin and rules

--- tests/code/FileCabinet/SuiteScripts/mySS.js
+++ tests/code/FileCabinet/SuiteScripts/mySS.js
@@ -1,14 +1,47 @@
 /* eslint-disable suitescript/api-version */
+
 /**
  * @NScriptType what
+ * // eslint-disable-next-line suitescript/api-version
  * @NApiVersion 2.2
- *
  */
 // eslint-disable-next-line suitescript/no-log-module,requirejs/no-js-extension
 define(['N/log', 'N/record', './mine.js', 'N/fake', 'module'], (log, record, mine, fake) => {
   log.debug({ title: 'hi', details: fake });
 
+  // eslint-disable-next-line no-unused-vars
+  const myO = {
+    // eslint-disable-next-line jsdoc/require-returns
+    /**
+     *
+     */
+    get a() {
+      return 'a';
+    }
+  };
+
+  // eslint-disable-next-line jsdoc/require-returns
+  /**
+   *
+   * @param a
+   * @param b
+   * @param c
+   * @param d
+   * @example
+   */
+  const aFn = (a, b, c, d) => ({ a, b, d });
+
+  /**
+   *
+   * @example
+   * @returns {undefined}
+   */
+  // eslint-disable-next-line no-unused-vars
+  function hi() {
+    return undefined;
+  }
+
   // eslint-disable-next-line no-global-assign,requirejs/no-assign-require
   require = util;
-  return { record, mine };
+  return { record, mine, aFn };
 });
